47fede5b7d352d8e0ae7c8155b34a69a7781f407
Shivaraj Umadi <102743170+shivau1208@users.noreply.github.com>
2023-11-22 18:00:50 +0530

    Update User.js with new schema methods

--- model/User.js
+++ model/User.js
@@ -2,16 +2,45 @@ const mongoose = require("mongoose");
 
 // Define a Mongoose schema
 const userSchema = new mongoose.Schema({
-  firstName: {
+  firstName: String,
+  age: Number,
+  email: {
     type: String,
+    minLength: 12,
+    unique: true,
+    required: true,
+    lowercase: true,
   },
-  Age: {
-    type: Number,
+  address: {
+    street: String,
+    city: String,
+    zipcode: Number,
   },
   createdAt: {
     type: Date,
+    immutable: true,
     default: Date.now(),
   },
+  updatedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+userSchema.methods.sayHi = function () {
+  console.log(`Hi... My name is ${this.firstName}`);
+};
+
+userSchema.statics.findByName = function (name) {
+  return this.where({ firstName: new RegExp(name, "i") });
+};
+
+userSchema.query.byName = function (name) {
+  return this.where({ firstName: new RegExp(name, "i") });
+};
+
+userSchema.virtual("namedEmail").get(function () {
+  return `${this.firstName} <${this.email}>`;
 });
 
 // Create a Mongoose model based on the schema
